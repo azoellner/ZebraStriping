d41ca81399dc0054a98d95a6c1a27e1f3083d98d
Andreas Zöllner <andzoell@web.de>
2023-09-10 00:37:07 +0200

    Add first version of the Thunderbird add-on

---
+++ README.md
@@ -0,0 +1,11 @@
+# Zebra Striping for Thunderbird
+
+This Thunderbird add-on adds zebra striping to the folder and message lists, i.e.,
+coloring of every other row of these lists to make them easier to read.
+
+## Technical Notes
+
+In older versions of Thunderbird, it was possible to achieve the zebra striping by
+[adding CSS rules](http://kb.mozillazine.org/Zebra_striping) to the user profile's
+_userChrome.css_ file. But with the changed implementation of the user interface in
+version 115, this no longer seems to be feasible.
---
+++ js/implementation.js
@@ -0,0 +1,164 @@
+const {ExtensionCommon} = ChromeUtils.import(
+    "resource://gre/modules/ExtensionCommon.jsm"
+);
+
+const {ExtensionSupport} = ChromeUtils.import(
+    "resource:///modules/ExtensionSupport.jsm"
+);
+
+const EXTENSION_ID = "zebra@az";
+
+const FOLDER_TREE = {
+    hoverColor: "var(--listbox-hover)",
+    oddOrEven: "odd",
+    selectedColor: "#D0D0D0",
+    zebraColor: "#F0F0F0",
+};
+const THREAD_TREE = {
+    hoverColor: "var(--listbox-hover)",
+    oddOrEven: "odd",
+    selectedColor: "#D0D0D0",
+    zebraColor: "#F0F0F0",
+};
+
+const findElementById = (window, elementId) => {
+    "use strict";
+    // For Thunderbird 115
+    for (const browser of window.document.querySelectorAll("browser")) {
+        if (browser.contentWindow !== null) {
+            const element = browser.contentWindow.document.getElementById(elementId);
+            if (element !== null) {
+                return element;
+            }
+        }
+    }
+    // For Thunderbird 102
+    return window.document.getElementById(elementId);
+};
+
+// Collect those HTML elements when created, in order to delete them on close
+let created_css = [];
+
+const addStyleElementToHeader = (tree) => {
+    "use strict";
+    const doc = tree.getRootNode();
+    const css = doc.createElement("style");
+    created_css.push(css);
+    doc.head.appendChild(css);
+    return css.sheet;
+};
+
+this.zebra_az = class extends ExtensionCommon.ExtensionAPI {
+    getAPI(context) {
+        context.callOnClose(this);
+        return {
+            zebra_az: {
+                init() {
+                    let updateFolderTreeIntervalID; // eslint-disable-line init-declarations
+                    let updateThreadTreeIntervalID; // eslint-disable-line init-declarations
+                    ExtensionSupport.registerWindowListener(EXTENSION_ID, {
+                        // Before Thunderbird 74, messenger.xhtml was messenger.xul
+                        chromeURLs: [
+                            "chrome://messenger/content/messenger.xhtml",
+                            "chrome://messenger/content/messenger.xul",
+                        ],
+                        onLoadWindow(window) {
+                            const TIMEOUT = 1000;
+
+                            // Thunderbird 115 takes a while for the folderTree to load, so retry until it is found.
+                            updateFolderTreeIntervalID = window.setInterval(() => {
+                                const tree = findElementById(window, "folderTree");
+                                if (tree !== null) {
+                                    try {
+                                        const sheet = addStyleElementToHeader(tree);
+                                        for (const rule of [
+                                            // The color for selected needs to be enforced for striped lines,
+                                            // and changed to contrast with striped lines
+                                            `
+li.selected > .container {
+    background-color: ${FOLDER_TREE.selectedColor};
+}
+`,
+                                            // The color for hovering needs to be changed to contrast with striping
+                                            `
+li:not(.selected) > .container:hover {
+    background-color: ${FOLDER_TREE.hoverColor};
+}
+`,
+                                            // And here's the zebra striping
+                                            `
+li:nth-child(${FOLDER_TREE.oddOrEven}) > .container {
+    background-color: ${FOLDER_TREE.zebraColor};
+}
+`,
+                                        ]) {
+                                            sheet.insertRule(rule);
+                                        }
+                                    } catch (error) {
+                                        console.error(error);
+                                    }
+
+                                    if (updateFolderTreeIntervalID !== undefined) {
+                                        window.clearInterval(
+                                            updateFolderTreeIntervalID
+                                        );
+                                        updateFolderTreeIntervalID = undefined;
+                                    }
+                                }
+                            }, TIMEOUT);
+
+                            updateThreadTreeIntervalID = window.setInterval(() => {
+                                const tree = findElementById(window, "threadTree");
+                                if (tree !== null) {
+                                    try {
+                                        const sheet = addStyleElementToHeader(tree);
+                                        for (const rule of [
+                                            // The color for selected needs to be enforced for striped lines,
+                                            // and changed to contrast with striping
+                                            `
+[is="tree-view-table-body"] > tr[is="thread-row"].selected {
+    background-color: ${THREAD_TREE.selectedColor};
+}
+`,
+                                            // The color for hovering needs to be enforced for striped lines
+                                            `
+[is="tree-view-table-body"] tr[is="thread-row"]:hover {
+    background-color: ${THREAD_TREE.hoverColor};
+}
+`,
+                                            // And here's the zebra striping
+                                            `
+[is="tree-view-table-body"] tr[is="thread-row"]:nth-child(${THREAD_TREE.oddOrEven}) {
+    background-color: ${THREAD_TREE.zebraColor};
+}
+`,
+                                        ]) {
+                                            sheet.insertRule(rule);
+                                        }
+                                    } catch (error) {
+                                        console.error(error);
+                                    }
+
+                                    if (updateThreadTreeIntervalID !== undefined) {
+                                        window.clearInterval(
+                                            updateThreadTreeIntervalID
+                                        );
+                                        updateThreadTreeIntervalID = undefined;
+                                    }
+                                }
+                            }, TIMEOUT);
+                        },
+                    });
+                },
+            },
+        };
+    }
+
+    close() {
+        ExtensionSupport.unregisterWindowListener(EXTENSION_ID);
+        for (const css of created_css) {
+            css.remove();
+        }
+        created_css = [];
+    }
+};
